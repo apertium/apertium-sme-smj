53d71826332ce4706ee2645eb82c34348b9fd844
Francis M. Tyers <ftyers@prompsit.com>
2009-06-18 15:54:32 +0000

    regression escript

--- regression-tests.sh
+++ regression-tests.sh
@@ -7,6 +7,12 @@ TSTLIST=`mktemp`;
 basedir=`pwd`;
 mode=sme-smj
 
+ECHO=echo
+
+if test x$(uname -s) = xDarwin; then 
+	ECHO=/opt/local/bin/gecho; 
+fi
+
 wget -O - -q http://wiki.apertium.org/wiki/Northern_Sami_and_Lule_Sami/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g' | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/!/g' | cut -f1 -d'!' | sed 's/(note:/!/g' | sed 's/_/ /g' | sed 's/$/./g' > $SRCLIST;
 wget -O - -q http://wiki.apertium.org/wiki/Northern_Sami_and_Lule_Sami/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g' | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/!/g' | cut -f2 -d'!' | sed 's/_/ /g' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/$/./g' > $TRGLIST;
 
@@ -21,26 +27,26 @@ CORRECT=0
 for LINE in `paste $SRCLIST $TRGLIST $TSTLIST | sed 's/ /%_%/g' | sed 's/\t/!/g'`; do
 #	echo $LINE;
 
-	SRC=`echo $LINE | sed 's/%_%/ /g' | cut -f1 -d'!' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/  / /g'`;
-	TRG=`echo $LINE | sed 's/%_%/ /g' | cut -f2 -d'!' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/  / /g'`;
-	TST=`echo $LINE | sed 's/%_%/ /g' | cut -f3 -d'!' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/  / /g'`;
+	SRC=`$ECHO $LINE | sed 's/%_%/ /g' | cut -f1 -d'!' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/  / /g'`;
+	TRG=`$ECHO $LINE | sed 's/%_%/ /g' | cut -f2 -d'!' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/  / /g'`;
+	TST=`$ECHO $LINE | sed 's/%_%/ /g' | cut -f3 -d'!' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/  / /g'`;
 
 	
-	echo $TRG | grep "^$TST$" > /dev/null;	
+	$ECHO $TRG | grep "^$TST$" > /dev/null;	
 	if [ $? -eq 1 ]; then
-		echo -e $mode"\t  "$SRC"\n\t- $TRG\n\t+ "$TST"\n";
+		$ECHO -e $mode"\t  "$SRC"\n\t- $TRG\n\t+ "$TST"\n";
 	else
-		echo -e $mode"\t  "$SRC"\nWORKS\t  $TST\n";
+		$ECHO -e $mode"\t  "$SRC"\nWORKS\t  $TST\n";
 		CORRECT=`expr $CORRECT + 1`;
 	fi
 	TOTAL=`expr $TOTAL + 1`;
 done
 
-echo $CORRECT" / "$TOTAL ;
+$ECHO $CORRECT" / "$TOTAL ;
 if [ -x /usr/bin/calc ]; then
 	WORKING=`calc $CORRECT" / "$TOTAL" * 100" | head -c 7`;
 
-	echo $WORKING"%";
+	$ECHO $WORKING"%";
 fi
 
 rm $SRCLIST $TRGLIST $TSTLIST;
