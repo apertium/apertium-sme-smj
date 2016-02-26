ba24bd4293e9491b190498516b7730c8cca77c1f
Sandra Rahka <sandra.rahka@gmail.com>
2016-02-26 10:55:47 +0000

    Smj: Lene and I commented back in line with dgen

--- check_mt-otpt.py
+++ check_mt-otpt.py
@@ -134,9 +134,9 @@ apertium_home=os.environ["APERTIUM_HOME"]
 
 atm_dir = apertium_home + '/apertium' + '-' + s_lang + '-' + t_lang
 #print("APT home is " + atm_dir)
-cmd = '| apertium -d ' + atm_dir + ' ' + s_lang + '-' + t_lang
+#cmd = '| apertium -d ' + atm_dir + ' ' + s_lang + '-' + t_lang
 # Change previous line to the following line if you want to see the hashform tags
-#cmd = "| apertium -d " + atm_dir + " " + s_lang + '-' + t_lang + '-dgen'
+cmd = "| apertium -d " + atm_dir + " " + s_lang + '-' + t_lang + '-dgen'
 
 def main():
     # parameters to be adjusted as needed
