5c8b70efd3d8a1dc0b992523d0a8a5b274b487f4
Lene Antonsen <lene.antonsen@uit.no>
2016-03-19 09:40:34 +0000

    sma smj smn: script: remove punctuation.

--- hash.sh
+++ hash.sh
@@ -1,1 +1,1 @@
-cat otpt_dir/*|grep 'tg-amt'|cut -d">" -f2-|cut -d"<" -f1|sed 's/&gt;/>/g;'|sed 's/&lt;/</g;'|tr ' ' '\n'|grep '^#'|sort|uniq -c|sort -nr > dev/hash-report.freq 
+cat otpt_dir/*|grep 'tg-amt'|cut -d">" -f2-|cut -d"<" -f1|sed 's/&gt;/>/g;'|sed 's/&lt;/</g;'|tr ' ' '\n'|grep '^#'|tr -d '.,"!:'|sort|uniq -c|sort -nr > dev/hash-report.freq 
