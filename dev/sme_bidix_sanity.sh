8ca0eb96d94970916d3ff525a22a471a0e3402ff
Lene Antonsen <lene.antonsen@uit.no>
2016-04-04 15:04:15 +0000

    sma smj smn: improved script.

--- dev/sme_bidix_sanity.sh
+++ dev/sme_bidix_sanity.sh
@@ -4,79 +4,79 @@
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep vblex |grep '"tv"' |cut -d '>' -f7 | sort -u > tvverbs 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep vblex |grep '"iv"' |cut -d '>' -f7 | sort -u > ivverbs
-cat tvverbs |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+TV' | cut -f2 | cut -d '+' -f1 |sort -u > gentv 
-cat ivverbs |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+IV' | cut -f2 | cut -d '+' -f1 |sort -u > geniv 
+cat tvverbs |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+TV' | cut -f2 | cut -d '+' -f1 |sort -u > gentv 
+cat ivverbs |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+IV' | cut -f2 | cut -d '+' -f1 |sort -u > geniv 
 echo 'The verb is not in the sme-FST or has wrong transitivity in dix:' > missingsme.txt
 echo '' >> missingsme.txt
-comm -23 tvverbs gentv | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
-comm -23 ivverbs geniv | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 tvverbs gentv | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 ivverbs geniv | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"n"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > nouns 
-cat nouns |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+N+' | cut -f2 | cut -d '+' -f1 |sort -u > gennouns 
+cat nouns |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+N+' | cut -f2 | cut -d '+' -f1 |sort -u > gennouns 
 echo '' >> missingsme.txt
 echo 'Nouns:' >> missingsme.txt
-comm -23 nouns gennouns | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 nouns gennouns | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"adv"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 |grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > advs 
-cat advs |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Adv' | cut -f2 | cut -d '+' -f1 |sort -u > genadvs 
+cat advs |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Adv' | cut -f2 | cut -d '+' -f1 |sort -u > genadvs 
 echo '' >> missingsme.txt
 echo 'Adverbs:' >> missingsme.txt
-comm -23 advs genadvs | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 advs genadvs | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"adj"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > adjs 
-cat adjs |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+A+' | cut -f2 | cut -d '+' -f1 |sort -u > genadjs 
+cat adjs |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+A+' | cut -f2 | cut -d '+' -f1 |sort -u > genadjs 
 echo '' >> missingsme.txt
 echo 'Adjectives:' >> missingsme.txt
-comm -23 adjs genadjs | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 adjs genadjs | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"post"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > post 
-cat post |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Po' | cut -f2 | cut -d '+' -f1 |sort -u > genpost 
+cat post |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Po' | cut -f2 | cut -d '+' -f1 |sort -u > genpost 
 echo '' >> missingsme.txt
 echo 'Post:' >> missingsme.txt
-comm -23 post genpost | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 post genpost | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"pr"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > pre 
-cat pre |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Pr' | cut -f2 | cut -d '+' -f1 |sort -u > genpre 
+cat pre |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Pr' | cut -f2 | cut -d '+' -f1 |sort -u > genpre 
 echo '' >> missingsme.txt
 echo 'Prep:' >> missingsme.txt
-comm -23 pre genpre | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 pre genpre | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"num"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > num 
-cat num |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Num' | cut -f2 | cut -d '+' -f1 |sort -u > gennum 
+cat num |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Num' | cut -f2 | cut -d '+' -f1 |sort -u > gennum 
 echo '' >> missingsme.txt
 echo 'Num:' >> missingsme.txt
-comm -23 num gennum | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 num gennum | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"prn"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > prn 
-cat prn |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Pron' | cut -f2 | cut -d '+' -f1 |sort -u > genprn 
+cat prn |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Pron' | cut -f2 | cut -d '+' -f1 |sort -u > genprn 
 echo '' >> missingsme.txt
 echo 'Pronouns:' >> missingsme.txt
-comm -23 prn genprn | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 prn genprn | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"cnjcoo"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > cnjcoo 
-cat cnjcoo |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+CC' | cut -f2 | cut -d '+' -f1 |sort -u > gencnjcoo 
+cat cnjcoo |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+CC' | cut -f2 | cut -d '+' -f1 |sort -u > gencnjcoo 
 echo '' >> missingsme.txt
 echo 'cnjcoo:' >> missingsme.txt
-comm -23 cnjcoo gencnjcoo | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 cnjcoo gencnjcoo | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"cnjsub"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > cnjsub 
-cat cnjsub |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+CS' | cut -f2 | cut -d '+' -f1 |sort -u > gencnjsub 
+cat cnjsub |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+CS' | cut -f2 | cut -d '+' -f1 |sort -u > gencnjsub 
 echo '' >> missingsme.txt
 echo 'cnjsub:' >> missingsme.txt
-comm -23 cnjsub gencnjsub | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 cnjsub gencnjsub | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"pcle"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > pcle 
-cat pcle |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Pcle' | cut -f2 | cut -d '+' -f1 |sort -u > genpcle 
+cat pcle |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Pcle' | cut -f2 | cut -d '+' -f1 |sort -u > genpcle 
 echo '' >> missingsme.txt
 echo 'Particles:' >> missingsme.txt
-comm -23 pcle genpcle | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 pcle genpcle | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 
 cat ../apertium-sme-smj.sme-smj.dix | tr '<' '>' | cut -d '>' -f1-10 |grep '"np"' |sed 's/>b\/>/%/g' |cut -d '>' -f7 | grep '[a-zščá][a-zščá]' |tr '%' ' ' | sort -u > proper 
-cat proper |$LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Prop' | cut -f2 | cut -d '+' -f1 |sort -u > genproper 
+cat proper |$LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst |grep '+Prop' | cut -f2 | cut -d '+' -f1 |sort -u > genproper 
 echo '' >> missingsme.txt
 echo 'Propernouns:' >> missingsme.txt
-comm -23 proper genproper | LOOKUP $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
+comm -23 proper genproper | LOOKUP -flags mbTT $GTHOME/langs/sme/src/analyser-gt-desc.xfst >> missingsme.txt
 rm  tvverbs ivverbs  gentv geniv nouns gennouns advs genadvs adjs genadjs post genpost pre genpre num gennum prn genprn cnjcoo gencnjcoo cnjsub gencnjsub pcle genpcle proper genproper  
 
 
