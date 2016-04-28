b64ee863964d16ee4d76afcadc327a451ac31786
Trond Trosterud <trond.trosterud@uit.no>
2016-04-28 12:57:39 +0000

    printf to be linux/mac-indepentent

--- dev/bidix-sanity.sh
+++ dev/bidix-sanity.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pair=`pwd | grep -o 'apertium-[a-z][a-z][a-z]-[a-z][a-z][a-z]'`;
-dir=`gecho $pair | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | gsed 's/^-//g'`; 
+dir=`printf "%s\n" "$pair" | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | gsed 's/^-//g'`; 
 dix=$pair.$dir.dix
-lang2=`gecho $dir | cut -f2 -d'-'`
-gtdir=`gecho $GTCORE | gsed 's/\(gtcore\|core\)//g'`;
+lang2=`printf "%s\n"  "$dir" | cut -f2 -d'-'`
+gtdir=`printf "%s\n"  "$GTCORE" | gsed 's/\(gtcore\|core\)//g'`;
 analysator=$gtdir"/langs/"$lang2"/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol"
 
 lt-expand ../$dix | gsed 's/\(<[^>]\+>\)\(<[^>]\+>\)\+/\1/g' | gsed 's/:[><]:/:/g'  | grep -v ':\([[:punct:]]\|[[:space:]]\)' | grep -v -- '-<' | grep -v '\/' | sort -u > /tmp/$dir.exp
@@ -14,18 +14,17 @@ cat /tmp/$dir.exp | cut -f2- -d':' | cut -f1 -d'<' | hfst-proc -w $analysator > 
 wc -l /tmp/$dir.exp /tmp/$dir.a
 
 for line in `paste /tmp/$dir.exp /tmp/$dir.a  | gsed 's/\t/:/g' | gsed 's/ /¬/g'`; do
-	lang1s=`gecho $line | cut -f1 -d':'`;
-	lang2s=`gecho $line | cut -f2 -d':'`;
-	lang2m=`gecho $line | cut -f3 -d':'`;
+	lang1s=`printf "%s\n"  "$line" | cut -f1 -d':'`;
+	lang2s=`printf "%s\n"  "$line" | cut -f2 -d':'`;
+	lang2m=`printf "%s\n"  "$line" | cut -f3 -d':'`;
 
-	num=`gecho "$lang2m" | grep -o "$lang2s" | wc -l`
+	num=`printf "%s\n"   "$lang2m" | grep -o "$lang2s" | wc -l`
 #	gecho $num" "$lang2m" "$lang2s;
 	if [[ $num -eq 0 ]]; then
-		gecho -e "%\t"$line;
+		printf "%%\t%s\n" "$line";
 	else
-#		gecho -e "+\t"$line;
+#		printf "%%\t%s\n" "$line";
 		continue
 	fi
 done
 	
-
