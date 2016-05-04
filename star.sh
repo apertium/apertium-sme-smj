ffc4037be3bd78408febad6717533ec3eae6c033
Lene Antonsen <lene.antonsen@uit.no>
2016-05-04 13:07:25 +0000

    corrected script

--- star.sh
+++ star.sh
@@ -1,3 +1,3 @@
 cat otpt_dir/*|grep 'tg-amt'|cut -d">" -f2|cut -d"<" -f1|tr ' ' '\n'|grep '^\*'|cut -c2-|tr -d '[.,:;)(?!]'|grep -v xxx |\
-lookup $GTHOME/langs/nob/src/analyser-gt-desc.xfst |\
-grep '?'|cut -f1|sort|uniq -c|sort -nr > dev/star-report.freq
+lookup $GTHOME/langs/sme/src/analyser-gt-desc.xfst |\
+grep -v '?'|cut -f1|sort|uniq -c|sort -nr > dev/star-report.freq
