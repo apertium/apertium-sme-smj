7cabbe42899f65a5104927df8a9ff399b509fe98
Lene Antonsen <lene.antonsen@uit.no>
2016-05-04 13:36:54 +0000

    smj: corrected script

--- star.sh
+++ star.sh
@@ -1,3 +1,2 @@
 cat otpt_dir/*|grep 'tg-amt'|cut -d">" -f2|cut -d"<" -f1|tr ' ' '\n'|grep '^\*'|cut -c2-|tr -d '[.,:;)(?!]'|grep -v xxx |\
-lookup $GTHOME/langs/sme/src/analyser-gt-desc.xfst |\
-grep -v '?'|cut -f1|sort|uniq -c|sort -nr > dev/star-report.freq
+sort|uniq -c|sort -nr > dev/star-report.freq
